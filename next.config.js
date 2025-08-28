2458473b52ae33a6b3a0ffa640853c9505541dc2
p6974875-cell <p6974875@gmail.com>
2025-08-28 03:55:52 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  experimental: {
+    appDir: true,
+  },
 };
+
 module.exports = nextConfig;
